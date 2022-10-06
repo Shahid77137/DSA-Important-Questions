ee265bb3591cef7885d00d76af86049ec9a06e02
Shahid Afridi <siddiquibabuddin202@gmail.com>
2022-10-06 11:45:20 +0530

    Question done

--- Interview/src/com/taliun/problem1.java
+++ Interview/src/com/taliun/problem1.java
@@ -3,44 +3,42 @@ package com.taliun;
 //write a program to reverse a String without ANY LOOP
 
 public class problem1 {
-public static void main(String[] args) {
-	String str = "abcd";
-	
-	StringBuilder revstr = new StringBuilder(str).reverse();
-	
-	System.out.println(revstr);
+    public static void main(String[] args) {
+        String str = "abcd";
 
+        StringBuilder revstr = new StringBuilder(str).reverse();
 
-	String input = "Hello, World!";
-   char[] chars = input.toCharArray();
-   int left = 0;
-   int right = chars.length - 1;
+        System.out.println(revstr);
 
-   while (left < right) {
-       // Swap characters at left and right indices
-       char temp = chars[left];
-       chars[left] = chars[right];
-       chars[right] = temp;
+        String input = "Hello, World!";
+        char[] chars = input.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
 
-       left++;
-       right--;
-   }
-    System.out.println(input);
-	String reversedString = new String(chars);
-	System.out.println(reversedString);
-//	String newstr = reverseStr(str);
-	
-	
-//	System.out.println(newstr);
+        while (left < right) {
+            // Swap characters at left and right indices
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
 
-}
-// public static String reverseStr(String str) {
-//	 
-//	 if(str.isEmpty()) {
-//		 return str;
-//	 }
-//	 
-//	return reverseStr(str.substring(1))+str.charAt(0);
-//	 
-// }
+            left++;
+            right--;
+        }
+        System.out.println(input);
+        String reversedString = new String(chars);
+        System.out.println(reversedString);
+        // String newstr = reverseStr(str);
+
+        // System.out.println(newstr);
+
+    }
+    // public static String reverseStr(String str) {
+    //
+    // if(str.isEmpty()) {
+    // return str;
+    // }
+    //
+    // return reverseStr(str.substring(1))+str.charAt(0);
+    //
+    // }
 }
