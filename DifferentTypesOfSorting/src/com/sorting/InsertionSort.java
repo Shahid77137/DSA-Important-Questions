e0ec1702bcf6129943aaef982f8c2464a9c8083a
Shahid Afridi <siddiquibabuddin202@gmail.com>
2022-10-10 11:45:20 +0530

    Question done

--- DifferentTypesOfSorting/src/com/sorting/InsertionSort.java
+++ DifferentTypesOfSorting/src/com/sorting/InsertionSort.java
@@ -3,27 +3,35 @@ package com.sorting;
 import java.util.Arrays;
 
 public class InsertionSort {
-public static void main(String[] args){
-    int[] arr = {10, 1, 7, 4, 8, 2, 11};
-    int n = arr.length;
-    sortByInsertion(arr, n);
-    System.out.println(Arrays.toString(arr));
-    for(int ele : arr){
-        System.out.print(ele + " ");
-    }
-}
-public static int[] sortByInsertion(int[] arr, int n){
-    for(int i=1;i<n;i++){
-        int temp = arr[i];
-        int j = i - 1;
-        for( ; j>=0;j--){
-            if(arr[j] > temp)
-            arr[j+1] = arr[j];
-            else 
-            break;
+    public static void main(String[] args) {
+        int[] arr = { 10, 1, 7, 4, 8, 2, 11 };
+        int n = arr.length;
+        sortByInsertion(arr, n);
+        System.out.println(Arrays.toString(arr));
+        for (int ele : arr) {
+            System.out.print(ele + " ");
         }
-      arr[j+1] = temp;
     }
-    return arr;
+
+    public static int[] sortByInsertion(int[] arr, int n) {
+        for (int i = 1; i < n; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (arr[j] > temp)
+                    arr[j + 1] = arr[j];
+                else
+                    break;
+            }
+            arr[j + 1] = temp;
+        }
+        return arr;
+    }
 }
-}
+
+// Time Complexity:
+// Worst-case: O(n^2)
+// Best-case: O(n) when the array is nearly sorted
+// Average-case: O(n^2)
+// Space Complexity: O(1) (In-place sorting)
+// Bubble Sort:
