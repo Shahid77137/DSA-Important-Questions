f3cac697d66f8928af2207b7b5baf980059b6682
Shahid Afridi <siddiquibabuddin202@gmail.com>
2022-10-11 11:45:20 +0530

    Question done

--- DifferentTypesOfSorting/src/com/sorting/InsertionSort.java
+++ DifferentTypesOfSorting/src/com/sorting/InsertionSort.java
@@ -1,5 +1,29 @@
 package com.sorting;
 
-public class InsertionSort {
+import java.util.Arrays;
 
+public class InsertionSort {
+public static void main(String[] args){
+    int[] arr = {10, 1, 7, 4, 8, 2, 11};
+    int n = arr.length;
+    sortByInsertion(arr, n);
+    System.out.println(Arrays.toString(arr));
+    for(int ele : arr){
+        System.out.print(ele + " ");
+    }
+}
+public static int[] sortByInsertion(int[] arr, int n){
+    for(int i=1;i<n;i++){
+        int temp = arr[i];
+        int j = i - 1;
+        for( ; j>=0;j--){
+            if(arr[j] > temp)
+            arr[j+1] = arr[j];
+            else 
+            break;
+        }
+      arr[j+1] = temp;
+    }
+    return arr;
+}
 }
