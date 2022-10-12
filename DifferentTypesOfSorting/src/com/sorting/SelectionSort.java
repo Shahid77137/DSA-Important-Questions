8ae9ab0030490a1fdd1c9c99899110acd594af41
Shahid Afridi <siddiquibabuddin202@gmail.com>
2022-10-12 11:45:20 +0530

    Question done

--- DifferentTypesOfSorting/src/com/sorting/SelectionSort.java
+++ DifferentTypesOfSorting/src/com/sorting/SelectionSort.java
@@ -2,7 +2,7 @@ package com.sorting;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] arr = { 5, 45, 15, 2, 11, 65 };
+        int[] arr = { 5, 45, 15, 2, 11, 65, 1};
         int n = arr.length;
         sortBySelection(arr, n); 
         
@@ -29,3 +29,27 @@ public class SelectionSort {
         arr[j] = temp;
     }
 }
+
+// Time Complexity:
+// Worst-case: O(n^2)
+// Best-case: O(n^2)
+// Average-case: O(n^2)
+// Space Complexity: O(1) (In-place sorting)
+
+// Swapping Two numbers without using third variable
+
+ class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        a = a + b; // Sum of a and b is stored in a
+        b = a - b; // Subtract b from the sum and store it in b (original 'a' value)
+        a = a - b; // Subtract the new 'b' value from the sum and store it in a (original 'b' value)
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
+
