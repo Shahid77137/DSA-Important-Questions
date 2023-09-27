a378ee48071400274f45c9010428ae5a542388bc
Shahid Afridi <siddiquibabuddin202@gmail.com>
2023-09-27 06:51:22 +0530

    Question done

--- 01_Fizz Buzz/Ideal Solutions/fizzBuzz.java
+++ 01_Fizz Buzz/Ideal Solutions/fizzBuzz.java
@@ -1,9 +1,9 @@
+import java.util.Scanner;
 // import java.lang.*;
 // import java.io.*;
 // import java.util.*;
 
-/*
- * class Main{
+ class Main{
 	//This function prints the elements according to the condition mentioned in the question.
 	public static void fizzBuzz(int n){
 		for (int i=1;i<=n;i++){
@@ -22,7 +22,47 @@
 	}
 }
 // 2nd Way of doing this
-import java.util.Scanner;
+
+class DemoMain {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        int n = Integer.parseInt(sc.next());
+        for (int i=1;i<=n;i++){
+            
+            if(i % 3 == 0 && i % 5 == 0)
+            System.out.println("FizzBuzz");
+            else if (i % 3 == 0)
+            System.out.println("Fizz");
+            else if (i % 5 == 0)
+            System.out.println("Buzz");
+            else
+            System.out.println(i);
+        }
+    }
+}
+
+
+class Main{
+	//This function prints the elements according to the condition mentioned in the question.
+	public static void fizzBuzz(int n){
+		for (int i=1;i<=n;i++){
+			if (i % 3 == 0 && i % 5 == 0) System.out.print("FizzBuzz" + " ");
+			else if (i % 3 == 0) System.out.print("Fizz" + " ");
+			else if (i % 5 == 0) System.out.print("Buzz" + " ");
+			else{
+				System.out.print(i + " ");
+			}
+		}
+	}
+	//Main Function
+	public static void main(String[] args){
+		int n = 10;
+		fizzBuzz(n);
+	}
+}
+// // 2nd Way of doing this
+// import java.util.Scanner;
 
 class Main {
     public static void main(String[] args){
@@ -42,47 +82,6 @@ class Main {
         }
     }
 }
- */
-
-// class Main{
-// 	//This function prints the elements according to the condition mentioned in the question.
-// 	public static void fizzBuzz(int n){
-// 		for (int i=1;i<=n;i++){
-// 			if (i % 3 == 0 && i % 5 == 0) System.out.print("FizzBuzz" + " ");
-// 			else if (i % 3 == 0) System.out.print("Fizz" + " ");
-// 			else if (i % 5 == 0) System.out.print("Buzz" + " ");
-// 			else{
-// 				System.out.print(i + " ");
-// 			}
-// 		}
-// 	}
-// 	//Main Function
-// 	public static void main(String[] args){
-// 		int n = 10;
-// 		fizzBuzz(n);
-// 	}
-// }
-// // 2nd Way of doing this
-// import java.util.Scanner;
-
-// class Main {
-//     public static void main(String[] args){
-//         Scanner sc = new Scanner(System.in);
-//         // int n = sc.nextInt();
-//         int n = Integer.parseInt(sc.next());
-//         for (int i=1;i<=n;i++){
-            
-//             if(i % 3 == 0 && i % 5 == 0)
-//             System.out.println("FizzBuzz");
-//             else if (i % 3 == 0)
-//             System.out.println("Fizz");
-//             else if (i % 5 == 0)
-//             System.out.println("Buzz");
-//             else
-//             System.out.println(i);
-//         }
-//     }
-// }
 // 3nd Way of doing this
 
 import java.util.Scanner;
--- Interview/src/com/taliun/DemoMain.java
+++ Interview/src/com/taliun/DemoMain.java
@@ -1,7 +1,10 @@
 package com.taliun;
-
+import java.util.Scanner;
 public class DemoMain {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.next());
+        double b = Double.parseDouble(sc.next());
         DemoExample obj = new DemoExample(3, "Fehmida", 24, 800000);
         // DemoMain obj = new DemoMain();
         System.out.println(obj.toString());
@@ -9,5 +12,6 @@ public class DemoMain {
         // obj.setId(4);
         System.out.println(obj.getAge());
         // int Id = obj.id;
+        sc.close();
     }
 }
